18206332f76f80ecebaa98f81ad2e274b7cfc014
PrekshaGupta <69909564+PrekshaGupta@users.noreply.github.com>
2020-12-03 18:51:32 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,53 +1,54 @@
-var car,wall;
+var thickness,wall;
 var bullet,speed,weight;
 
 function setup() {
   createCanvas(1600,400);
-  car = createSprite(50, 200, 50, 50);
-  wall = createSprite(1500,200,60,height/2);
+  bullet = createSprite(50, 200, 50, 50);
+  wall = createSprite(1500,200,thickness,height/2);
 
-  wall.shapeColor = color(80,80,80);
+  wall.shapeColor = color(230,230,230);
 
-  speed = random(30,52);
-  weight = random(223,321);
+  weight = random(30,52);
+  speed = random(223,321);
   thickness = random(22,83);
 
-  car.velocityX = speed;
+  bullet.velocityX = speed;
+  bullet.shapeColor=color(255);
 }
 
 function draw() {
-  background(255);
+  background(0);
 
-  if(wall.x - car.x < (car.width + wall.width)/2){
-   car.velocityX = 0;
+  if(wall.x - bullet.x < (bullet.width + wall.width)/2){
+   bullet.velocityX = 0;
    var deformation = 0.5 * weight * speed * speed/22500;
 
    if(deformation > 180){
-     car.shapeColor = color(255,0,0);
+     bullet.shapeColor = color(255,0,0);
    }
    
    if(deformation < 180 && deformation > 100){
-     car.shapeColor = color(230,230,0);
+     bullet.shapeColor = color(230,230,0);
    }
 
    if(deformation < 100){
-     car.shapeColor = color(0,255,0);
+     bullet.shapeColor = color(0,255,0);
    }
   }
 
   if(hasCollided(bullet, wall)){
     bullet.velocityX = 0;
-    var damage = 0.5*weight*speed*/thickness*thickness*thickness;
+    var damage = 0.5*weight*speed*speed/(thickness*thickness*thickness);
     
     if(damage > 10){
       wall.shapeColor = color(255,0,0);
     }
   
     if(damage < 10){
-      wall.shapeColor = color(255,0,0);
+      wall.shapeColor = color(0,255,0);
     }
   
-    car.hasCollided(wall);
+    bullet.hasCollided(wall);
 
   }
 
@@ -55,7 +56,7 @@ function draw() {
 }
 
 function hasCollided(bullet,wall){
-  bulletRightEdge = bullet.x + bullet.width;
+  bulletRightEdge = bullet.x + bullet.width/2;
   wallLeftEdge = wall.x;
   
   if(bulletRightEdge>=wallLeftEdge){
